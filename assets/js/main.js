2c5cee080684447dac6380fc6d4a28594de0b49a
Alisher <alisher.bazhenov88@gmail.com>
2024-03-27 01:48:17 +0300

    result

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log("Hello");
+let dropdownBlock = document.querySelector(".dropdown-social-icons");
+let listItems = document.querySelectorAll(".item-dropdown");
+
+let menuVisible = false;
+
+dropdownBlock.addEventListener("mouseover", () => {
+  if (!menuVisible) {
+    listItems.forEach(function (item, index) {
+      if (index > 1) {
+        item.classList.remove("item-dropdown-close");
+      }
+    });
+    menuVisible = true;
+  }
+});
+
+dropdownBlock.addEventListener("mouseout", () => {
+  if (menuVisible) {
+    listItems.forEach(function (item, index) {
+      if (index > 1) {
+        item.classList.add("item-dropdown-close");
+      }
+    });
+    menuVisible = false;
+  }
+});
