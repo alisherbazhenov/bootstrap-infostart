12ed1494c3d2036b2124336167bdc86e99aebdca
Alisher <alisher.bazhenov88@gmail.com>
2024-03-27 14:07:07 +0300

    weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,14 @@ dropdownBlock.addEventListener("mouseout", () => {
     menuVisible = false;
   }
 });
+
+// const api =
+//   "http://api.openweathermap.org/geo/1.0/reverse?lat=59.9342802&lon=30.3350986&limit=1&appid=cb0283d74a6165dfdf6a409c850b68bf";
+
+// fetch(api)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     document.querySelector(".locaton").textContent = data.name;
+//   })
+//   .catch(() => {});
