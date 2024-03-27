252647b75db91e58df80a664e075b3107f3188a5
Alisher <alisher.bazhenov88@gmail.com>
2024-03-27 15:02:26 +0300

    weather-witget

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,24 @@ dropdownBlock.addEventListener("mouseout", () => {
   }
 });
 
-// const api =
-//   "http://api.openweathermap.org/geo/1.0/reverse?lat=59.9342802&lon=30.3350986&limit=1&appid=cb0283d74a6165dfdf6a409c850b68bf";
+const api =
+  "http://api.openweathermap.org/geo/1.0/reverse?lat=59.9342802&lon=30.3350986&limit=1&appid=cb0283d74a6165dfdf6a409c850b68bf";
 
-// fetch(api)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     document.querySelector(".locaton").textContent = data.name;
-//   })
-//   .catch(() => {});
+fetch(api)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    if (data && data.name) {
+      console.log(data);
+      document.querySelector(".location").textContent = data.name;
+    } else {
+      throw new Error("Данные не доступны");
+    }
+  })
+  .catch((error) => {
+    console.error("Ошибка получения данных:", error.message);
+  });
